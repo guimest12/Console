e690383cf29c997845d0be82a3ad80bb134eac12
guimest12 <132007130+guimest12@users.noreply.github.com>
2023-07-27 21:42:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+let firstName = "Guilherme";
+let lastName = "Paz";
+
+
+let fullName = firstName.concat(" ", lastName);
+
+
+console.log("Nome completo: " + fullName);
+
+
+let num1 = 10;
+let num2 = 5;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+
+console.log("Soma: " + sum);
+console.log("Diferença: " + difference);
+console.log("Produto: " + product);
+console.log("Quociente: " + quotient);
